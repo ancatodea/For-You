d282ebe0a45d595f5d9b7c7b51fe0e53afe3abb7
ancatodea <103606786+ancatodea@users.noreply.github.com>
2023-05-28 21:47:54 +0200

    foryou exercises

--- app.js
+++ app.js
@@ -10,17 +10,17 @@
 
 // var numar = prompt('Introduceti un numar');
 
-// function checkFizzBuzz(nr) {
-//   if (nr % 15 == 0 ) {
-//       console.log('FizzBuzz');
-//   } else if (nr % 5 == 0) {
-//       console.log('Buzzz' );
-//    } else if (nr % 3 == 0) {
-//      console.log('Fizz');
-//    } else {
-//      console.log(nr);
-//    }
-// }
+function checkFizzBuzz(nr) {
+  if (nr % 15 == 0 ) {
+      console.log('FizzBuzz');
+  } else if (nr % 5 == 0) {
+      console.log('Buzzz' );
+   } else if (nr % 3 == 0) {
+     console.log('Fizz');
+   } else {
+     console.log(nr);
+   }
+}
 
 // var checkVar = checkFizzBuzz(numar);
 
@@ -35,57 +35,57 @@
 // b. Acelasi lucru, facut cu if
 
 
-// var rezultatMeci = 'x';
+var rezultatMeci = 'x';
 
-// function winner(rez) {
+function winner(rez) {
 
-//   switch(rez) {
-//     case '1' :
-//       console.log('Gazdele au castigat');
-//       break;
-//     case 'x' :
-//       console.log('Egalitate');
-//       break;
-//     case '2' :
-//       console.log('Oaspetii au castigat');
-//       break;
-//     default :
-//     console.log('Valoare invalida');
-//   }
-// }
+  switch(rez) {
+    case '1' :
+      console.log('Gazdele au castigat');
+      break;
+    case 'x' :
+      console.log('Egalitate');
+      break;
+    case '2' :
+      console.log('Oaspetii au castigat');
+      break;
+    default :
+    console.log('Valoare invalida');
+  }
+}
+
+// var winnerResult = winner(rezultatMeci);
+
+var rezMeci = '1';
 
 
-// var rezMeci = '1';
+function winner2(rez) {
+  if (rez == '1') {
+    console.log('Gazdele au castigat');
+  } else if (rez == 'x') {
+    console.log('Egalitate');
+  }else if (rez == 'X') {
+    console.log('Egalitate');
+  }else if (rez == '2') {
+    console.log('Oaspetii au castigat');
+  }else {
+    console.log('Valoare invalida');
+  }
+}
 
+// var checkWinner = winner2(rezMeci);
 
-// function winner2(rez) {
-//   if (rez == '1') {
-//     console.log('Gazdele au castigat');
-//   } else if (rez == 'x') {
-//     console.log('Egalitate');
-//   }else if (rez == 'X') {
-//     console.log('Egalitate');
-//   }else if (rez == '2') {
-//     console.log('Oaspetii au castigat');
-//   }else {
-//     console.log('Valoare invalida');
-//   }
-// }
+// TERNARY:
 
-// var afisareRezultate = winner2(rezMeci);
-// // var checkWinner = winner(rezultatMeci);
+// Se da o variabila care reprezinta varsta unui utilizator. 
+// Verificati (in functie de varsta) ce bautura poate comanda un utilizator - alcoolica sau nu
 
-// // TERNARY:
+var varsta = 20;
 
-// // // Se da o variabila care reprezinta varsta unui utilizator. 
-// // Verificati (in functie de varsta) ce bautura poate comanda un utilizator - alcoolica sau nu
+function checkAge (age) {
 
-// var varsta = 20;
-
-// function checkAge (age) {
-
-//   age >= 18 ? console.log('Puteti comanda si bauturi alcolice') : console.log('Nu aveti voie sa consumati alcool');
-// }
+  age >= 18 ? console.log('Puteti comanda si bauturi alcolice') : console.log('Nu aveti voie sa consumati alcool');
+}
 
 // var rezultatVarsta = checkAge(varsta);
 
@@ -96,22 +96,10 @@
 
 // a) stiind ca hotelul poate primi maxim 15 caini, implementati o conditie care verifica daca mai sunt locuri disponibile pentru caini si afisati in consola mesaje corespunzatoare
 
-// b) stiind ca hotelul poate primi maxim 30 de animale in acelasi timp, implementati o conditie care verifica daca hotelul este supra-aglomerat
-
-// c) pentru fiecare pisica trebuie sa fie disponibile cate 3 jucarii, iar pentru fiecare caine cate 5. Calculati si afisati cate jucarii trebuie sa cumpere cei de la hotel pentru animale.
-
-// d) daca numarul total al animalelor este mai mic de 20 sau sunt mai multi de 5 angajati, consideram ca animalele sunt bine ingrijite, implementati o conditie care sa verifice si sa afiseze in consola mesaje corespunzatoare
-
-// e) numarul norocos al patronului este 7. Daca la orice moment cand viziteaza hotelul (este si el acolo!) numarul total al fiintelor din interior este multiplu de 7, fiecare angajat va primi un bonus de 100 de lei. Calculati si afisati in consola cati bani il va costa o astfel de vizita.
-
-// f) daca avem un numar impar de caini si un numar impar de pisici, afisam "Paritate SI"
-
-// g) daca avem un numar impar de caini sau un numar impar de pisici, afisam "Paritate Sau"
-
-
 var numberOfDogs = 12;
 var numerOfCats = 5;
 var numberOfEmployees = 3;
+var numberOfPets = numberOfDogs + numerOfCats;
 
 
 function capacityDogs (dogs) {
@@ -125,6 +113,65 @@ function capacityDogs (dogs) {
 
 var resultCapacityDogs = capacityDogs(numberOfDogs);
 
-// b) stiind ca hotelul poate primi maxim 30 de animale in acelasi timp, implementati o conditie care verifica daca hotelul este supra-aglomerat
+// // b) stiind ca hotelul poate primi maxim 30 de animale in acelasi timp, implementati o conditie care verifica daca hotelul este supra-aglomerat
 
-function maxCapacity ()
+function maxCapacity (pets) {
+  var allPets = 30 - pets;
+
+  if (pets  >= 30  ) {
+    console.log ('We have full house, right now there are no free spots');
+  } else {
+    console.log('we have ', allPets, 'spots left');
+  }
+}
+
+var checkCapacity = maxCapacity(numberOfPets);
+
+// c) pentru fiecare pisica trebuie sa fie disponibile cate 3 jucarii, iar pentru fiecare caine cate 5. Calculati si afisati cate jucarii trebuie sa cumpere cei de la hotel pentru animale.
+
+function availableToys (dogs, cats) {
+  var toys = (5 *  dogs) + (3 * cats);
+  console.log('Right now we need ', toys, 'toys');
+}
+
+var checkToys = availableToys(numberOfDogs, numerOfCats);
+
+// d) daca numarul total al animalelor este mai mic de 20 sau sunt mai multi de 5 angajati, consideram ca animalele sunt bine ingrijite, implementati o conditie care sa verifice si sa afiseze in consola mesaje corespunzatoare
+
+function careWell (pets, employees) {
+  if ((pets < 20) || (employees > 5)) {
+    console.log('All pets get the proper care')
+  } else {
+    console.log('We could use an extra pair of hands')
+  }
+}
+
+var checkCare = careWell(numberOfPets, numberOfEmployees);
+
+// e) numarul norocos al patronului este 7. Daca la orice moment cand viziteaza hotelul (este si el acolo!) numarul total al fiintelor din interior este multiplu de 7, fiecare angajat va primi un bonus de 100 de lei. Calculati si afisati in consola cati bani il va costa o astfel de vizita.
+
+function visitBoss (pets, employees) {
+  var ownerHere = true;
+  if ((ownerHere == true) && (pets % 7 == 0)) {
+    var bonusEmployees = 100 * employees;
+    console.log('The total bonus is', bonusEmployees);
+  } else {
+    console.log('There is no match for you to get bonus today');
+  }
+}
+
+var checkBonus = visitBoss(numberOfPets, numberOfEmployees);
+
+// f) daca avem un numar impar de caini si un numar impar de pisici, afisam "Paritate SI" 
+// g) daca avem un numar impar de caini sau un numar impar de pisici, afisam "Paritate Sau"
+
+function oddNumberPets (dogs, cats) {
+  if ((dogs % 2 == 1) && (cats % 2 == 1)){
+    console.log('Imparitate Si');
+  } else if ((dogs % 2 == 1) || (cats % 2 == 1)){
+    console.log('Imparitate Sau');
+  }
+}
+
+var checkOddNumberPets = oddNumberPets(numberOfDogs, numerOfCats);
+
